c2bb9545f41d74a103217ba7aaa1709d8253ef70
bogdan <trifan.bogdan09@yahoo.com>
2024-11-16 12:36:35 +0200

    Instalare Entity Framework Core

--- proiect-daw/obj/Debug/net8.0/proiect-daw.AssemblyInfo.cs
+++ proiect-daw/obj/Debug/net8.0/proiect-daw.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proiect-daw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cceab50eb33ba34df35ae0d96f9cf0113c9c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("proiect-daw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proiect-daw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
